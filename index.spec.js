874f0e05c85cdd3bb688da79a992ef098285e133
stiven <stiven2750@gmail.com>
2020-08-28 20:32:06 -0500

    update spec

--- index.spec.js
+++ index.spec.js
@@ -4,7 +4,7 @@ describe('testing index', () => {
 
     test('debe retornar un numero', () => {
 
-        const result = suma(1, 10);
+        const result = suma(1, 20);
         expect(result).toEqual(11);
 
     });
