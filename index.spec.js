20a3207068c724e9b86e4190eef90e956a5da8a7
stiven <stiven2750@gmail.com>
2020-08-28 20:05:33 -0500

    change one

--- index.spec.js
+++ index.spec.js
@@ -4,7 +4,7 @@ describe('testing index', () => {
 
     test('debe retornar un numero', () => {
 
-        const result = suma(1, 2);
+        const result = suma(1, 5);
         expect(typeof result).toBe('number');
 
     });
